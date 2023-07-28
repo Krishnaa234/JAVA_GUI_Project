3018ee318aab9140b23100d5626cdf1d114f3f2c
Krishna Jangid <108576349+Krishnaa234@users.noreply.github.com>
2023-07-29 01:27:22 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,92 @@
+package bank.management.system;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Login extends JFrame implements ActionListener {
+    
+    JButton login, signup, clear;
+    JTextField cardtext;
+    JPasswordField pintext; //not visible entered number but like * is visible instead
+    
+    Login() {
+        setTitle("ATM");
+        
+        setLayout(null);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg")); //fetch the image on that address
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT); //get the image i1 to i2 so that it can get scaled or resized
+        //can not pass image i2 to JLabel without converting it into ImageIcon
+        ImageIcon i3 = new ImageIcon(i2); //image i2 to ImageIcon i3
+        JLabel label = new JLabel(i3);
+        label.setBounds(70, 10, 100, 100); //custom the size
+        add(label); //added label on frame 
+        
+        JLabel text = new JLabel("Welcome to ATM");
+        text.setFont(new Font("OSWARD", Font.BOLD, 38));
+        text.setBounds(200, 40,400, 40);
+        add(text);
+        
+        JLabel cardno = new JLabel("Card Number:");
+        cardno.setFont(new Font("Raleway", Font.BOLD, 28));
+        cardno.setBounds(120, 150,300, 40);
+        add(cardno);
+        
+        cardtext = new JTextField();
+        cardtext.setBounds(350, 160, 250, 30);
+        cardtext.setFont(new Font("Arial", Font.BOLD, 14));
+        add(cardtext);
+        
+        JLabel pin = new JLabel("PIN:");
+        pin.setFont(new Font("Raleway", Font.BOLD, 28));
+        pin.setBounds(120, 220,250, 30);
+        add(pin);
+        
+        pintext = new JPasswordField();
+        pintext.setBounds(350, 220, 250, 30);
+        pintext.setFont(new Font("Arial", Font.BOLD, 14));
+        add(pintext);
+        
+        login = new JButton("SIGN IN");
+        login.setBounds(300, 300, 100, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.addActionListener(this); //to catch the event of the button
+        add(login);
+        
+        clear = new JButton("CLEAR");
+        clear.setBounds(430, 300, 100, 30);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        clear.addActionListener(this); //to catch the event of the button
+        add(clear);
+        
+        signup = new JButton("SIGN UP");
+        signup.setBounds(300, 350, 230, 30);
+        signup.setBackground(Color.BLACK);
+        signup.setForeground(Color.WHITE);
+        signup.addActionListener(this); //to catch the event of the button
+        add(signup);
+        
+        getContentPane().setBackground(Color.WHITE);
+        setSize(800, 480); //frame dimensions are set
+        setVisible(true);
+        setLocation(350, 200); //display frame at this coordinate
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        //to catch which button is pressed or clicked
+        if(ae.getSource() == clear) {
+            cardtext.setText("");
+            pintext.setText("");
+        }
+        else if(ae.getSource() == login) {
+            
+        }
+        else if(ae.getSource() == signup) {
+            
+        }
+    }
+    
+    public static void main(String args[]) {
+        new Login(); //anonomous object which calls the constructor after formation 
+    }
+}
